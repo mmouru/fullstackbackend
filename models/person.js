4b08b273aeb37c57c1c74caeb6eed53e6fb1ce26
mmouru <marttimourujarvi@gmail.com>
2020-03-27 20:50:32 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -16,8 +16,8 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   })
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true, minlength: 3, maxlength: 8},
-  number: {type: String, required: true}
+  name: { type: String, required: true, unique: true, minlength: 3},
+  number: {type: String, required: true, minlength: 8}
 })
 
 personSchema.plugin(uniqueValidator)
